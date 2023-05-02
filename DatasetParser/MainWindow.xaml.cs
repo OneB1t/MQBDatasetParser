4e0508415a30cfea207437140421656191625c7b
Jan Žďárský <jzdarsky@simpleway.cz>
2023-05-02 20:17:44 +0200

    improvements

--- DatasetParser/MainWindow.xaml.cs
+++ DatasetParser/MainWindow.xaml.cs
@@ -7,6 +7,15 @@ using System.Xml.Serialization;
 using System.Xml;
 using Xml2CSharp;
 using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections;
+using DiffPlex;
+using DiffPlex.DiffBuilder.Model;
+using DiffPlex.Model;
+using DiffPlex.Chunkers;
+using DiffPlex.Wpf.Controls;
 
 namespace DatasetParser
 {
@@ -15,7 +24,6 @@ namespace DatasetParser
     /// </summary>
     public partial class MainWindow : Window
     {
-        string filePath;
         public MainWindow()
         {
             InitializeComponent();
@@ -42,9 +50,13 @@ namespace DatasetParser
 
                     // Display the binary data in the TextBox
                     binaryDataTextBox.Text = response?.RESULT.RESPONSE.DATA.PARAMETER_DATA.Text.Replace("0x", "").Replace(","," ");
+                    sw_name.Text = response?.RESULT.RESPONSE.DATA.PARAMETER_DATA.ZDC_NAME;
+                    sw_version.Text = response?.RESULT.RESPONSE.DATA.PARAMETER_DATA.ZDC_VERSION;
 
                     string[] hexValuesSplit = response?.RESULT.RESPONSE.DATA.PARAMETER_DATA.Text.Split(',');
                     byte[] datasetBytes = new byte[hexValuesSplit.Length];
+
+    
 
                     for (int i = 0; i < hexValuesSplit.Length; i++)
                     {
@@ -58,13 +70,78 @@ namespace DatasetParser
                     crc32calculation.Text = CRC32Reversed.Compute(dataForCRC).ToString("X");
 
                     // Set the file path text
-                    filePathTextBlock.Text = openFileDialog.FileName;
+                    filePathTextBlock.Text = openFileDialog.SafeFileName;
+                    this.DataContext = this;
                 }
                 catch (Exception ex)
                 {
                     MessageBox.Show("Error: " + ex.Message);
                 }
             }
+        }
+
+        private void loadFileButton2_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "XML Files (*.xml)|*.xml";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    // Load the XML file into an XmlDocument object
+                    XmlDocument xmlDoc = new XmlDocument();
+                    xmlDoc.Load(openFileDialog.FileName);
+
+                    // Deserialize the XML document into a Response object using an XmlSerializer
+                    XmlSerializer serializer = new XmlSerializer(typeof(MESSAGE));
+                    MESSAGE? response;
+                    using (var reader = new StringReader(xmlDoc.InnerXml))
+                    {
+                        response = serializer.Deserialize(reader) as MESSAGE;
+                    }
+                    binaryDataTextBox2.Text = response?.RESULT.RESPONSE.DATA.PARAMETER_DATA.Text.Replace("0x", "").Replace(",", " ");
+                    string[] hexValuesSplit = response?.RESULT.RESPONSE.DATA.PARAMETER_DATA.Text.Split(',');
+                    byte[] datasetBytes = new byte[hexValuesSplit.Length];
+
+ 
+
+                    for (int i = 0; i < hexValuesSplit.Length; i++)
+                    {
+                        datasetBytes[i] = Convert.ToByte(hexValuesSplit[i], 16);
+                    }
+
+                    // Set the file path text
+                    filePathTextBlock2.Text = openFileDialog.SafeFileName;
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+        private string AddLinesToResult(string input)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                sb.Append(input[i]);
+
+                if ((i + 1) % 128 == 0)
+                {
+                    sb.AppendLine();
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        private void compareFilesButton_Click(object sender, RoutedEventArgs e)
+        {
+            DiffView.OldText = AddLinesToResult(binaryDataTextBox.Text);
+            DiffView.NewText = AddLinesToResult(binaryDataTextBox2.Text);
+            DiffView.IgnoreUnchanged = true;
         }
 
         private void saveFileButton_Click(object sender, RoutedEventArgs e)
@@ -82,6 +159,11 @@ namespace DatasetParser
         {
             int selectedByteIndex = binaryDataTextBox.SelectionStart / 3;
             selectedByteTextBlock.Text = $"Selected byte index: {selectedByteIndex}";
+        }
+        private void binaryDataTextBox2_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            int selectedByteIndex = binaryDataTextBox2.SelectionStart / 3;
+            selectedByteTextBlock2.Text = $"Selected byte index: {selectedByteIndex}";
         }
     }
 }
