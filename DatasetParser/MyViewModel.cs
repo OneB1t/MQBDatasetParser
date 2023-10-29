0c131e6aa282412147b77ad25574fa9f32942492
Jan Žďárský <jzdarsky@simpleway.cz>
2023-10-29 13:11:01 +0100

    tweaks

---
+++ DatasetParser/MyViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace DatasetParser
+{
+    public class MyViewModel : INotifyPropertyChanged
+    {
+        private string fileNamePath;
+
+        public string FileNamePath
+        {
+            get { return fileNamePath; }
+            set
+            {
+                if (fileNamePath != value)
+                {
+                    fileNamePath = value;
+                    OnPropertyChanged(nameof(FileNamePath));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
